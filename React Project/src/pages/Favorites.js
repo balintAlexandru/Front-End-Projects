a7a3ff646611e1118b70299c44402ce36a30cd0c
Alexandru <balintalex737@gmail.com>
2021-12-19 16:01:59 +0200

    Add React Project

---
+++ React Project/src/pages/Favorites.js
@@ -0,0 +1,13 @@
+
+import FavoriteCard from '../components/favorite/FavoriteCard';
+
+function FavoritesPage(){
+    return(
+        <div>
+        <FavoriteCard text = "Favorite Card 1"/>
+        <FavoriteCard text = "Favorite Card 2"/>
+        <FavoriteCard text = "Favorite Card 3"/>
+        </div>
+    );
+}
+export default FavoritesPage;
